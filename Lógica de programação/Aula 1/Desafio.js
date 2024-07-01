70e0275cf972981248182b0ba174e5d1e33af0c6
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-01 19:12:27 -0300

    Commit 0

---
+++ Lógica de programação/Aula 1/Desafio.js
@@ -0,0 +1,12 @@
+alert('Boas Vindas ao nosso site!')
+let nome = "Lua"
+let idade = 25
+let numeroDeVendas = 50
+alert('Erro! Preencha todos os campos')
+let mensagemDeErro = 'Erro! Preencha todos os campos'
+alert(mensagemDeErro)
+nome = prompt('Qual seu nome, usuário?')
+idade = prompt('Qual sua idade, usuário?')
+if (idade >= 18){
+    alert('Pode tirar a habilitação!')
+}
---
+++ Lógica de programação/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao nosso site!')
+const numeroSecreto = Math.floor(Math.random()*5+1)
+if (prompt('manda ai') == numeroSecreto) {
+    console.log('acerto miseravi')
+} else {
+    console.log(`Se é burro é? O número era ${numeroSecreto}`)
+}
+
