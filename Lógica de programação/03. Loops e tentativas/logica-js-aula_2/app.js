65c1775396fcf043d8a0c0d1e591440eb1600645
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-03 15:47:47 -0300

    Aula 3 Pronta + Aula 1 organizada

--- Lógica de programação/03. Loops e tentativas/logica-js-aula_2/app.js
+++ Lógica de programação/03. Loops e tentativas/logica-js-aula_2/app.js
@@ -1,11 +1,18 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
-
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
-}
+let chute
+let tentativas = 1
+do {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else if (numeroSecreto > chute) {
+        alert(`${chute} é menor do que o numero secreto`)
+    } else if (numeroSecreto < chute) {
+        alert(`${chute} é maior do que o número secreto`)
+    }
+    if (numeroSecreto != chute) {
+        tentativas++
+    }
+} while (numeroSecreto != chute)
