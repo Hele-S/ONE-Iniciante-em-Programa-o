5f73bf82926cf51750baa84a15846bf6b9c289e4
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-07 15:40:12 -0300

    explore funções e listas SETUP

---
+++ Lógica de programação/Mergulhe em programação com JavaScript/05.Desafio/app.js
@@ -0,0 +1,21 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 50
+let numeroSecreto = Math.floor(Math.random()*numeroMaximo+1);
+console.log(numeroSecreto)
+let chute
+let tentativas = 1
+do {
+    chute = prompt('Escolha um número entre 1 e ' + numeroMaximo);
+    if (chute == numeroSecreto) {
+        break;
+    } else if (numeroSecreto > chute) {
+        alert(`${chute} é menor do que o numero secreto`)
+    } else if (numeroSecreto < chute) {
+        alert(`${chute} é maior do que o número secreto`)
+    }
+    if (numeroSecreto != chute) {
+        tentativas++
+    }
+} while (numeroSecreto != chute)
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
